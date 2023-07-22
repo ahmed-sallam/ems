9012c634f4f53295986efd8367a4ee6b8d6b0a24
ahmed-sallam <devahmedsallam@gmail.com>
2023-07-22 21:02:28 +0300

    update department service implementation

--- src/main/java/com/techpeak/ems/company/services/impl/DepartmentServiceImpl.java
+++ src/main/java/com/techpeak/ems/company/services/impl/DepartmentServiceImpl.java
@@ -2,39 +2,63 @@ package com.techpeak.ems.company.services.impl;
 
 import com.techpeak.ems.company.dto.DepartmentDto;
 import com.techpeak.ems.company.dto.DepartmentResDto;
+import com.techpeak.ems.company.dto.mapper.DeparmentMapper;
 import com.techpeak.ems.company.entities.DepartmentEntity;
+import com.techpeak.ems.company.repositories.DepartmentRepository;
 import com.techpeak.ems.company.services.DepartmentService;
+import com.techpeak.ems.exceptions.NotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
+@RequiredArgsConstructor
 public class DepartmentServiceImpl implements DepartmentService {
+    private final DepartmentRepository repository;
+    private final DeparmentMapper mapper;
+
     @Override
     public List<DepartmentResDto> listDepartments() {
-        return null;
+        List<DepartmentEntity> entities = repository.findAll();
+        return mapper.toListDto(entities);
     }
 
     @Override
     public DepartmentResDto findDepartmentById(Long id) {
-        return null;
+
+        DepartmentEntity entity = findOrThrow(id);
+        return mapper.toDto(entity);
     }
 
     @Override
     public DepartmentResDto createDepartment(DepartmentDto dto) {
-        return null;
+
+        DepartmentEntity entity = mapper.toEntity(dto);
+        DepartmentEntity newEntity = repository.save(entity);
+        return mapper.toDto(newEntity);
     }
 
     @Override
     public void deleteDepartmentById(Long id) {
-
+        findOrThrow(id);
+        repository.deleteById(id);
     }
 
     @Override
     public DepartmentResDto updateDepartment(Long id, DepartmentDto dto) {
-        return null;
+        DepartmentEntity entity = findOrThrow(id);
+        entity.setName(dto.getName());
+        DepartmentEntity newEntity = repository.save(entity);
+        return mapper.toDto(newEntity);
     }
 
     @Override
     public DepartmentEntity findOrThrow(Long id) {
-        return null;
+        return repository
+                .findById(id)
+                .orElseThrow(()->
+                        new NotFoundException("Department With ID" + id + " " +
+                                "does not exist"));
     }
 }
